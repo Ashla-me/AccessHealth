a52b06026db9d4dcebbbc7816823fe5029b22ed9
Ashla-me <mercyashamesunday@gmail.com>
2024-07-15 22:22:43 +0100

    work update

--- app.py
+++ app.py
@@ -9,15 +9,13 @@ from routes.appointments import appointments_bp
 from routes.telehealth import telehealth_bp
 from flask_cors import CORS
 import requests
-import logging
 
-app = Flask(__name__, static_folder='web-static', template_folder='template')
+app = Flask(__name__, static_folder='static', template_folder='template')
 
 app.config['SECRET_KEY'] = 'ae3ecc52f525b916cb484cd7cc74c077c7ab04f0651206d6'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:ASHla1212!@localhost/bite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = '861af9062c612f4352007ea17eb8c1545857418b3007e36b024daec9bf7861c5'
-logging.basicConfig(level=logging.DEBUG)
 
 jwt = JWTManager(app)
 db.init_app(app)
@@ -35,8 +33,15 @@ with app.app_context():
 @app.route('/')
 def home():
     print('Welcome to AccessHealth!')
-    app.logger.debug('Rendering home page')
     return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/patients', methods=['GET'])
 def get_patients():
